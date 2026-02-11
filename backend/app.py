1cf17c45095f13201b13a478f9b37fbfa6701bc8
ryan <k0etsu@k0etsu.com>
2026-02-11 16:14:45 -0500

    update backend port

--- backend/app.py
+++ backend/app.py
@@ -515,4 +515,4 @@ def get_results(category_id):
     return results
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5001)
